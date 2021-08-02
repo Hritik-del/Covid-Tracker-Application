171ef1ff606329b12514fb48e8941c056c4c6af7
Hritik-del <74361880+Hritik-del@users.noreply.github.com>
2021-08-02 13:25:25 +0530

    Covid Tracker App

---
+++ app/src/main/java/com/example/covid_19tracker/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.covid_19tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.leo.simplearcloader.SimpleArcLoader;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.StringReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Now connect all the textView, ScrollView, piechart etc, that I have created
+    // in activity_main.xml to Main_Activity. (Reason - so that we can set data to make them work.
+    TextView tvCases, tvRecovered, tvCritical, tvActive, tvTodayCases, tvTotalDeaths, tvTodayDeaths, tvAffectedCountries;
+    SimpleArcLoader simpleArcLoader;
+    ScrollView scrollView;
+    PieChart pieChart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //connection starts here.
+        tvCases = findViewById(R.id.tvCases);
+        tvRecovered = findViewById(R.id.tvRecovered);
+        tvCritical = findViewById(R.id.tvCritical);
+        tvActive = findViewById(R.id.tvActive);
+        tvTodayCases = findViewById(R.id.tvTotalCases);
+        tvTotalDeaths = findViewById(R.id.tvTotalDeaths);
+        tvTodayDeaths = findViewById(R.id.tvTodayDeaths);
+        tvAffectedCountries = findViewById(R.id.tvAffectedCountries);
+
+        simpleArcLoader = findViewById(R.id.loader);
+        scrollView = findViewById(R.id.scrollStats);
+        pieChart = findViewById(R.id.piechart);
+
+        // Now calling a function for getting data from api.
+        fetchData();
+    }
+
+    // Most important part, here we are going to use RESTAPI.
+    //Here we are getting data from corona.lmao.ninja API.
+    // We used PostMan software for exploiting data from the api
+    private void fetchData() {
+
+        String url = "https://corona.lmao.ninja/v2/all/";
+
+        simpleArcLoader.start();
+
+        StringRequest request = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Whatever data from Json we get will come to this method/
+                        try {
+                            JSONObject jsonObject = new JSONObject((response.toString()));
+
+                            tvCases.setText(jsonObject.getString("cases"));
+                            tvRecovered.setText(jsonObject.getString("recovered"));
+                            tvCritical.setText(jsonObject.getString("critical"));
+                            tvActive.setText(jsonObject.getString("active"));
+                            tvTodayCases.setText(jsonObject.getString("todayCases"));
+                            tvTotalDeaths.setText(jsonObject.getString("deaths"));
+                            tvTodayDeaths.setText(jsonObject.getString("todayDeaths"));
+                            tvAffectedCountries.setText(jsonObject.getString("affectedCountries"));
+
+                            // Now show data in pie, the methods which we use here are form the documentation of the pie which
+                            // imported to gradle from git
+                            pieChart.addPieSlice(new PieModel("Cases", Integer.parseInt(tvCases.getText().toString()), Color.parseColor("#FFA726")));
+                            pieChart.addPieSlice(new PieModel("Recovered", Integer.parseInt(tvRecovered.getText().toString()), Color.parseColor("#66BB6A")));
+                            pieChart.addPieSlice(new PieModel("Deaths", Integer.parseInt(tvTotalDeaths.getText().toString()), Color.parseColor("#EF5350")));
+                            pieChart.addPieSlice(new PieModel("Active", Integer.parseInt(tvActive.getText().toString()), Color.parseColor("#29B6F6")));
+                            pieChart.startAnimation();
+
+                            // Now when all our data is fetched and set to the text view and piechart now
+                            // we stop our arc loader and set its visibility gone and make the visibility of scroll view visible
+                            simpleArcLoader.stop();
+                            simpleArcLoader.setVisibility(View.GONE);
+                            scrollView.setVisibility(View.VISIBLE);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            simpleArcLoader.stop();
+                            simpleArcLoader.setVisibility(View.GONE);
+                            scrollView.setVisibility(View.VISIBLE);
+
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                simpleArcLoader.stop();
+                simpleArcLoader.setVisibility(View.GONE);
+                scrollView.setVisibility(View.VISIBLE);
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+
+    }
+
+    public void goTrackCountries(View view) {
+
+        startActivity(new Intent(getApplicationContext(), AffectedCountries.class));
+    }
+}
+
+/* NOTES
+Before getting data from api dont forget to give internet permissions in Android Manifest.
+To get an icon like search then right click drawable -> new -> Vector and find the icon
+ */
